6f199b05ab75ae5ad07ff0f0911cb393bc23a312
Charles <CMcFeaters@gmail.com>
2014-06-04 17:27:14 -0400

    working on tables

--- budg_tables.py
+++ budg_tables.py
@@ -3,7 +3,7 @@ budg_tables
 this contains all of the setup data for the tables
 '''
 from appHolder import db
-import unittest, datetime, inspect, types
+import datetime, inspect, types
 import sys, string, os
 
 '''remove these lines when done with debugging
@@ -157,7 +157,33 @@ class Expense(db.Model):
 	def __iter__(self):
 		#iterate the members of cashflow
 		return iter([attr[0] for attr in inspect.getmembers(self,not inspect.ismethod) if type(attr[1])!=types.MethodType and not attr[0].startswith("_")])
-		
+
+
+class Transfer(db.Model):
+	'''transfer is an expense from one account to another
+	ex: paying your credit card bill, cc account decreases, debit account decreases
+		f_account_id is the from account
+		t_account_id is the to account
+		value will be relative to the from account, e.g. if -500 is the value
+		f_account will have entVal+value
+		t_account_id will have entVal-val
+		'''
+	__tablename__="transfers"
+	
+	id=db.Column(db.Integer,primary_key=True)
+	f_account_id=db.Column(db.Integer,db.ForeignKey('accounts.id'))
+	t_account_id=db.Column(db.Integer,db.ForeignKey('acconts.id'))
+	title=db.Column(db.String)
+	value=db.Column(db.Integer)
+	date=db.Column(db.DateTime)
+	
+	def __init__(self,f_account_id,t_account_id,title,value,date):
+		self.f_account_id=f_account_id
+		self.t_account_id=t_account_id
+		self.title=title
+		self.value=value
+		self.date=date
+	
 class Account(db.Model):
 	'''primary account class.  the account is setup with a title, a starting value, a starting date and a low value (used to execute warnings)
 	additionally cashflows can be linked to the account (separate table) and will be accessed by the account to display output values
@@ -248,120 +274,3 @@ class Account(db.Model):
 	def __iter__(self):
 		return iter([attr[0] for attr in inspect.getmembers(self,not inspect.ismethod) if type(attr[1])!=types.MethodType and not attr[0].startswith("_") and not len(attr[0])<=1])
 		
-class AccountTests(unittest.TestCase):
-	#x=Account("Test",10,goalVal=100,goalDate=datetime.date(2014,1,28))
-	#create tests for engine/db based development
-
-	
-	#def createDB(self):
-
-	
-	#db.create_all()
-	#engine=create_engine('sqlite:///'+path,echo=False)
-	#Session=sessionmaker(bind=engine)
-	#session=Session()#instance of a session to communicate with the DB
-	#Base.metadata.create_all(engine)	#create the DB with tables
-	#	return session
-	
-	def deleteDB(self):
-		path="C:\\Users\Charles\Dropbox\Programming\DataBases\\budget.db"
-		if os.path.exists(path):
-			os.remove(path)
-	
-	def test001_AddAccount(self):		
-		#create an account
-		acc=Account("Checking", 100, datetime.datetime(2014,01,01),5)
-		#session=self.createDB()
-		db.session.add(acc)
-		db.session.commit()	#add the entry to the db
-		self.assertTrue(True)
-		
-	def test002_AddCashFlows(self):
-		#add a cashflow
-		#session=self.createDB()
-		acc=db.session.query(Account).filter(Account.title=="Checking").all()[0]
-		rent=CashFlow(acc.id,"Rent",-10,datetime.datetime(2014,02,01),"Month",1,datetime.datetime(2015,01,20))
-		income=CashFlow(acc.id,"Payroll",2048,datetime.datetime(2014,01,23),"Week",2,datetime.datetime(2015,01,20))
-		#expense2=CashFlow(acc.id,"Drug Sales",-100,datetime.date(2014,01,23),estimate=True)#,"Day",1,datetime.date(2014,1,30))
-		db.session.add(rent)
-		db.session.add(income)
-		#db.session.add(expense2)
-		db.session.commit()
-		#session.close()
-		self.assertTrue(True)
-		
-	def test003_AccountBalance(self):
-		#check the account balance of the account
-		#session=self.createDB()
-		result=Account.query
-		acc=result.all()[0]
-		print "Value: "+str(acc.getDateValue())
-		#session.close()
-
-		self.assertTrue(True)
-	
-	def test004_CashFlowRate(self):
-		#check the cashflow rate between two given dates
-		#session=self.createDB()
-		result=db.session.query(Account)
-		acc=result.all()[0]
-		print "Daily: "+str(acc.getRate("Day",datetime.datetime(2014, 1,21),datetime.datetime(2014,2,23)))
-		print "Weekly: "+str(acc.getRate("Week",datetime.datetime(2014, 1,21),datetime.datetime(2014,2,23)))
-		print "Monthly: "+str(acc.getRate("Month",datetime.datetime(2014, 1,21),datetime.datetime(2014,2,23)))
-		#session.close()
-		self.assertTrue(True)
-	
-	def test005_upcomingExpenses(self):
-		#return a list of upcoming expenses from today till X
-		#session=self.createDB()
-		result=db.session.query(Account)
-		acc=result.all()[0]
-		
-		for bill in acc.getPayments(datetime.datetime(2014,3,24)):
-			print "Title: "+bill.title+" Date: "+str(bill.date)+" - Amount: "+str(bill.value)
-		
-		#session.close()
-		self.assertTrue(True)
-	
-	def test006_AccountDetectManualMiss(self):
-		#given an estimated cashflow, an account needs to be able to check if the cashflow has been updated
-		#an updated cashflow will have estimate moved to false
-		#this should just be updating bits
-		acc=db.session.query(Account).all()[0]
-		#add the income to be estimated
-		print acc.getEstimates(datetime.datetime.today())
-		
-	def _test007_popTest(self):
-		#tests the pop function of cashflow
-		
-		acc=db.session.query(Account).all()[0]
-		print acc.cashFlows[0].popSeries()
-		self.assertTrue(True)
-	
-	def _test999_deleteDB(self):
-		db.session.close()
-		self.deleteDB()
-		print "deleted"
-		self.assertTrue(True)
-		
-	def printAccounts(self):
-		results=db.session.query(Account)
-		
-		for thing in results:
-			print "_____******______"
-			print thing
-			print "_____******______"
-
-		
-def main():
-	from flask import Flask
-	from flask.ext.sqlalchemy import SQLAlchemy
-	path="Users/Charles/Dropbox/Programming/DataBases/budget.db"
-	app = Flask(__name__)
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+path
-	db = SQLAlchemy(app)
-	db.create_all()
-	unittest.main()
-	
-if __name__=="__main__":
-	main()
